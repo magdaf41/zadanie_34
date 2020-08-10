c295de9d1984017ca005d8d1b59b3608134ffc5b
Magdalena Falkowska-Legieć <magda.f41@gmail.com>
2020-08-10 19:19:18 +0200

    zadanie_34 - poprawione

--- main.js
+++ main.js
@@ -7,12 +7,11 @@ let n2 = text2.length;
 function compareStr(n1, n2) {
     let result = n1 == n2;
     if (n1 > n2) {
-        console.log(text1 + " - " + n1);
+        return(text1 + " - " + n1);
     } else {
-        console.log(text2 + " - " + n2);
+        return(text2 + " - " + n2);
     }
-    return result;
 }
-compareStr(n1, n2);
+console.log(compareStr(n1, n2));
 
 
